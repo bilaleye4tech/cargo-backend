5c25ce78a701225df40291e55a780be78f5b400c
Muhammad Bilal <144024650+bilaleye4tech@users.noreply.github.com>
2024-08-19 12:08:00 +0500

    remove code

--- app/Http/Middleware/Authenticate.php
+++ app/Http/Middleware/Authenticate.php
@@ -12,7 +12,6 @@ class Authenticate extends Middleware
      */
     protected function redirectTo($request)
     {
-        // return $request->expectsJson() ? null : route('login');
         return $request->expectsJson() ? null : '';
     }
 }
